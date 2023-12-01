982bbc3598899dff7484d3ac44026a94ce71ae03
Pradee Banara <pradeepbandara0918@gmail.com>
2023-12-01 07:15:09 +0530

    adedd emp, add emp ui, saved details in db

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,18 +9,21 @@ class Employee extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'name',
-        'email',
-        'birth_date',
-        'gender',
         'employee_id',
-        'company',
-        'holidays',
-        'leaves',
-        'clients',
-        'projects',
-        'tasks',
-        'assets',
-        'timing_sheets',
+        'f_name',
+        'l_name',
+        'full_name',
+        'employee_id',
+        'email',
+        'dob',
+        'nic',
+        'c_number',
+        'j_title',
+        'd_name',
+        'joinedDate',
+        'createdDate',
+        'status',
+        'gender',
+        'description',
     ];
 }
