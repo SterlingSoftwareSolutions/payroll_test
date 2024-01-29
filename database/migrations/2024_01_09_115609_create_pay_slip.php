8232225ccec793d78ece8e89d6c9eec285cdadfd
Naseef <AUNaseef@users.noreply.github.com>
2024-01-29 13:39:52 +0530

    payslip database updates

--- database/migrations/2024_01_09_115609_create_pay_slip.php
+++ database/migrations/2024_01_09_115609_create_pay_slip.php
@@ -16,20 +16,40 @@ return new class extends Migration
         Schema::create('pay_slip', function (Blueprint $table) {
             $table->id();
             $table->string('employee_id')->constrained()->nullOnDelete();
+            // net_salary - computed field
+            $table->boolean('approved')->default(false);
+
+            // Gross salary
             $table->double('basic_salary');
-            $table->double('net_salary');
-            $table->double('epf');
-            $table->double('etf');
-            $table->double('ot_pay');
+            $table->double('br_allowance');
+            $table->double('fixed_allowance');
+            $table->double('attendance_allowance');
+
+            // Deductions
+            $table->double('no_pay_leave_deduction');
             $table->double('late_deduction');
-            $table->double('p_a_y_e');
+
+            $table->double('employee_epf');
+            $table->double('paye');
             $table->double('stamp_duty');
+
+            $table->double('advance');
+            $table->double('loan');
+
+            // Extra paymments
             $table->double('holiday_payment');
-            $table->double('advance_payment');
+            $table->double('incentives');
+
+            // Company EPF & ETF
+            $table->double('company_epf');
+            $table->double('etf');
+
+            // Bank account details
             $table->string('account_name');
             $table->double('account_number');
             $table->string('bank_name');
             $table->string('branch');
+
             $table->timestamps();
         });
     }
